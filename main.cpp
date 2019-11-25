7396d38de816e5fc30e5f8c1945c48b46e44a4f8
Fernando Brasil <colaboradores@evah.io>
2019-11-25 20:32:37 -0300

    Ajustes do trabalho Colocado comentarios Adicionar funcao de stopwords

--- main.cpp
+++ main.cpp
@@ -1,248 +1,226 @@
-// CPP program to display content of Trie
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define alpha_size 26
+#define tamanho_alfabeto 26
 
-// Converts key current character into index
-// use only 'a' through 'z' and lower case
-#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+#define char_int(c) ((int)c - (int)'a')
 using namespace std;
-const int ALPHABET_SIZE = 26;
 
-// Trie node
-struct TrieNode
+//Estrutura representante do no
+struct noTrie
 {
-    struct TrieNode *children[alpha_size];
-
-    bool isLeaf;
+    struct noTrie *filho[tamanho_alfabeto];
+    bool folha;
 };
 
-// Returns new trie node (initialized to NULLs)
-struct TrieNode *getNode(void)
+//Cria um novo No
+//Pre-condicao: Nenhuma
+//Pos-condicao: Retorna um novo no
+struct noTrie *novoNo(void)
 {
-    struct TrieNode *pNode = new TrieNode;
-    pNode->isLeaf = false;
+    struct noTrie *pNode = new noTrie;
+    pNode->folha = false;
 
-    for (int i = 0; i < ALPHABET_SIZE; i++)
-        pNode->children[i] = NULL;
+    for (int i = 0; i < tamanho_alfabeto; i++)
+        pNode->filho[i] = NULL;
 
     return pNode;
 }
-// Returns true if key presents in trie, else
-// false
-bool search(struct TrieNode *root, string key)
+//Verifica se a palavra existe
+//Pre-condicao: Arvore nao vazia
+//Pos-condicao: nenhuma
+//Retorno: true ou false se a palavra existe
+bool palavra_existe(struct noTrie *raiz, string palavra)
 {
-    struct TrieNode *pCrawl = root;
+    struct noTrie *temp = raiz;
 
-    for (int i = 0; i < key.length(); i++)
+    for (int i = 0; i < palavra.length(); i++)
     {
-        int index = key[i] - 'a';
-        if (!pCrawl->children[index])
+        int index = palavra[i] - 'a';
+        if (!temp->filho[index])
             return false;
 
-        pCrawl = pCrawl->children[index];
+        temp = temp->filho[index];
     }
 
-    return (pCrawl != NULL && pCrawl->isLeaf);
+    return (temp != NULL && temp->folha);
 }
 
-// If not present, inserts key into trie.  If the
-// key is prefix of trie node, just marks leaf node
-void insert(struct TrieNode *root, const string key)
+//Insere a palavra na arvore
+//Pre-condicao: String nao vazia
+//Pos-condicao: Palavra inserida na estrutura
+void inserir(struct noTrie *raiz, const string palavra)
 {
-    struct TrieNode *pCrawl = root;
+    struct noTrie *temp = raiz;
     int index;
 
-    for (int level = 0; level < key.length(); level++)
+    for (int level = 0; level < palavra.length(); level++)
     {
-        index = CHAR_TO_INDEX(key[level]);
-        cout << index << endl;
-        if (!pCrawl->children[index])
-            pCrawl->children[index] = getNode();
+        index = char_int(palavra[level]);
+        
+        if (!temp->filho[index])
+            temp->filho[index] = novoNo();
 
-        pCrawl = pCrawl->children[index];
+        temp = temp->filho[index];
     }
 
-    // mark last node as leaf
-
-    pCrawl->isLeaf = true;
+    temp->folha = true;
 }
 
-// function to check if current node is leaf node or not
-bool isLeafNode(struct TrieNode *root)
+//Verifica se o no é folha
+//Pre-condicao: Raiz nao pode ser nulla
+//Pos-condicao: true ou false caso seja ou nao
+bool eh_folha(struct noTrie *raiz)
 {
-    return root->isLeaf != false;
+    return raiz->folha != false;
 }
 
-// Returns 0 if current node has a child
-// If all children are NULL, return 1.
-bool isLastNode(struct TrieNode *root)
+//Verifica se é o ultimo No
+//Pre-condicao: No nao pode ser nulla
+//Pos-condicao: o ou 1 caso seja ou nao
+bool eh_ultimo_no(struct noTrie *no)
 {
-    for (int i = 0; i < ALPHABET_SIZE; i++)
-        if (root->children[i])
+    for (int i = 0; i < tamanho_alfabeto; i++)
+        if (no->filho[i])
             return 0;
     return 1;
 }
 
-// Returns true if root has no children, else false
-bool isEmpty(TrieNode *root)
+//Verifica se o no é vazio
+//Pre-condicao: No nao pode ser nulla
+//Pos-condicao: true ou false caso seja vazio ou nao
+bool vazio(noTrie *no)
 {
-    for (int i = 0; i < ALPHABET_SIZE; i++)
-        if (root->children[i])
+    for (int i = 0; i < tamanho_alfabeto; i++)
+        if (no->filho[i])
             return false;
     return true;
 }
 
-// Recursive function to delete a key from given Trie
-TrieNode *remove(TrieNode *root, string key, int depth = 0)
+//Remove palavra da arvore
+//Pre-condicao: String e raiz nao pode ser nulla
+//Pos-condicao: Palavra removida da estrutura
+noTrie *remover(noTrie *raiz, string palavra, int level = 0)
 {
-    // If tree is empty
-    if (!root)
+    if (!raiz)
         return NULL;
 
-    // If last character of key is being processed
-    if (depth == key.size())
+    if (level == palavra.size())
     {
 
-        // This node is no more end of word after
-        // removal of given key
-        if (root->isLeaf)
-            root->isLeaf = false;
+        if (raiz->folha)
+            raiz->folha = false;
 
-        // If given is not prefix of any other word
-        if (isEmpty(root))
+        if (vazio(raiz))
         {
-            delete (root);
-            root = NULL;
+            delete (raiz);
+            raiz = NULL;
         }
 
-        return root;
+        return raiz;
     }
 
-    // If not last character, recur for the child
-    // obtained using ASCII value
-    int index = key[depth] - 'a';
-    root->children[index] =
-        remove(root->children[index], key, depth + 1);
+    int index = palavra[level] - 'a';
+    raiz->filho[index] =
+        remover(raiz->filho[index], palavra, level + 1);
 
-    // If root does not have any child (its only child got
-    // deleted), and it is not end of another word.
-    if (isEmpty(root) && root->isLeaf == false)
+    if (vazio(raiz) && raiz->folha == false)
     {
-        delete (root);
-        root = NULL;
+        delete (raiz);
+        raiz = NULL;
     }
 
-    return root;
+    return raiz;
 }
 
-// function to display the content of Trie
-void display(struct TrieNode *root, char str[], int level)
+//Imprimi todas as palavras que estao na arvore
+//Pre-condicao: No nao pode ser nulla
+//Pos-condicao: Imprimi todos as palavras da arvore
+void imprimir(struct noTrie *raiz, char str[], int level)
 {
-    // If node is leaf node, it indicates end
-    // of string, so a null character is added
-    // and string is displayed
-    if (isLeafNode(root))
+    if (eh_folha(raiz))
     {
         str[level] = '\0';
         cout << str << endl;
     }
 
     int i;
-    for (i = 0; i < alpha_size; i++)
+    for (i = 0; i < tamanho_alfabeto; i++)
     {
-        // if NON NULL child is found
-        // add parent key to str and
-        // call the display function recursively
-        // for child node
-        if (root->children[i])
+        if (raiz->filho[i])
         {
             str[level] = i + 'a';
-            display(root->children[i], str, level + 1);
+            imprimir(raiz->filho[i], str, level + 1);
         }
     }
 }
 
-// Recursive function to print auto-suggestions for given
-// node.
-void suggestionsRec(struct TrieNode *root, string currPrefix)
+//Funcao auxiliar recursiva para mostrar palavra
+//Pre-condicao: Raiz nao pode ser nulla
+//Pos-condicao: Mostra palavra
+void consultar_palavra_recursiva(struct noTrie *raiz, string palavra)
 {
-    // found a string in Trie with the given prefix
-    if (root->isLeaf)
+    if (raiz->folha)
     {
-        cout << currPrefix;
+        cout << palavra;
         cout << endl;
     }
 
-    // All children struct node pointers are NULL
-    if (isLastNode(root))
+    if (eh_ultimo_no(raiz))
         return;
 
-    for (int i = 0; i < ALPHABET_SIZE; i++)
+    for (int i = 0; i < tamanho_alfabeto; i++)
     {
-        if (root->children[i])
+        if (raiz->filho[i])
         {
-            // append current character to currPrefix string
-            currPrefix.push_back(97 + i);
-
-            // recur over the rest
-            suggestionsRec(root->children[i], currPrefix);
-
-            currPrefix.pop_back();
+            palavra.push_back(97 + i);
+            consultar_palavra_recursiva(raiz->filho[i], palavra);
+            palavra.pop_back();
         }
     }
 }
 
-// print suggestions for given query prefix.
-int printAutoSuggestions(TrieNode *root, const string query)
+//Funcao para mostrar palavra
+//Pre-condicao: raiz nao pode ser nula
+//Pos-condicao: Mostra palavra
+int consultar_palavra(noTrie *raiz, const string palavra)
 {
-    struct TrieNode *pCrawl = root;
+    struct noTrie *temp = raiz;
 
-    // Check if prefix is present and find the
-    // the node (of last level) with last character
-    // of given string.
     int level;
-    int n = query.length();
+    int n = palavra.length();
     for (level = 0; level < n; level++)
     {
-        int index = CHAR_TO_INDEX(query[level]);
+        int index = char_int(palavra[level]);
 
-        // no string in the Trie has this prefix
-        if (!pCrawl->children[index])
+        if (!temp->filho[index])
             return 0;
 
-        pCrawl = pCrawl->children[index];
+        temp = temp->filho[index];
     }
 
-    // If prefix is present as a word.
-    bool isWord = (pCrawl->isLeaf == true);
+    bool ehpalavra = (temp->folha == true);
 
-    // If prefix is last node of tree (has no
-    // children)
-    bool isLast = isLastNode(pCrawl);
+    bool ehFolha = eh_ultimo_no(temp);
 
-    // If prefix is present as a word, but
-    // there is no subtree below the last
-    // matching node.
-    if (isWord && isLast)
+    if (ehpalavra && ehFolha)
     {
-        cout << query << endl;
+        cout << palavra << endl;
         return -1;
     }
 
-    // If there are are nodes below last
-    // matching character.
-    if (!isLast)
+    if (!ehFolha)
     {
-        string prefix = query;
-        suggestionsRec(pCrawl, prefix);
+        string prefixo = palavra;
+        consultar_palavra_recursiva(temp, prefixo);
         return 1;
     }
 }
-
-void inserir_via_arquivo_txt(TrieNode *root)
+//Inserir palavras via arquivo txt
+//Pre-condicao: Arquivo deve existir
+//Pos-condicao: Grava todas as palavras na arvore
+void inserir_via_arquivo_txt(noTrie *raiz)
 {
 
     FILE *file1;
@@ -261,7 +239,36 @@ void inserir_via_arquivo_txt(TrieNode *root)
         while (fscanf(file1, "%[^\n]%*c", palavra) != EOF)
         {
             /*  cout << "Palavra no arquivo: " << palavra << endl; */
-            insert(root, palavra);
+            inserir(raiz, palavra);
+        }
+    }
+    printf("LEITURA FEITA COM SUCESSO\n");
+
+    fclose(file1);
+}
+//Funcao para remover palavras contidas no arquivo
+//Pre-condicao: raiz nao pode nula
+//Pos-condicao: Remove palavras
+void stopwords(noTrie *raiz)
+{
+
+    FILE *file1;
+    file1 = fopen("stopwords.txt", "r");
+
+    char type;
+    char palavra[200];
+
+    if (file1 == NULL)
+    {
+        printf("Problemas na LEITURA do arquivo\n");
+    }
+    else
+    {
+        printf("ARQUIVO ABERTO COM SUCESSO LEITURA SENDO FEITA ...\n");
+        while (fscanf(file1, "%[^\n]%*c", palavra) != EOF)
+        {
+            /*  cout << "Palavra no arquivo: " << palavra << endl; */
+           remover(raiz,palavra);
         }
     }
     printf("LEITURA FEITA COM SUCESSO\n");
@@ -269,7 +276,48 @@ void inserir_via_arquivo_txt(TrieNode *root)
     fclose(file1);
 }
 
-void ClearTerminal()
+bool CheckSimilares(string palavraPrinciapl, string PalavraTeste, int distancia) {
+    if (distancia >= 0) {
+    	if( palavraPrinciapl.length() == 0 && PalavraTeste.length() == 0) 
+    		return true;
+        if ( palavraPrinciapl.length() == 0 ) 
+        	return (PalavraTeste.length() <= distancia);
+        if ( PalavraTeste.length() == 0 ) 
+        	return (palavraPrinciapl.length() <= distancia);
+        char a[1];  a[0]  = palavraPrinciapl[0];
+        char b[1];  b[0]  = PalavraTeste[0];
+        if (strcmp(a,b) == 0) 
+            return CheckSimilares(palavraPrinciapl.substr(1, palavraPrinciapl.length()), PalavraTeste.substr(1, PalavraTeste.length()), distancia);
+        else
+            return CheckSimilares(palavraPrinciapl, PalavraTeste.substr(1, PalavraTeste.length()), distancia-1);
+    }
+    return false;
+}
+
+
+
+void imprimirSimilar(struct noTrie *raiz, char str[], int level,int distancia, char palavra[]){
+    if (eh_folha(raiz)){
+        if(CheckSimilares(palavra,str,distancia)){
+            str[level] = '\0';
+            cout << str << endl;
+        }
+    }
+    int i;
+    for (i = 0; i < tamanho_alfabeto; i++){
+        if (raiz->filho[i]){
+            str[level] = i + 'a';
+            imprimirSimilar(raiz->filho[i], str, level + 1,distancia,palavra);
+        }
+    }
+}
+
+
+
+//Limpa terminal
+//Pre-condicao: Nenhuma
+//Pos-condicao: Limpa o terminal
+void limparTerminal()
 {
 #ifdef _WIN32
     system("cls");
@@ -278,29 +326,14 @@ void ClearTerminal()
 #endif
 }
 
-// Driver program to test above functions
 int main()
 {
 
     int level = 0;
     char str[50];
 
-    // Displaying content of Trie
-    //cout << "Conteudo da trie " << endl;
-
-    //display(root, str, level);
-
-    //cout << "Removendo" << endl;
-    //remove(root, "neuropatia");
-    //remove(root, "Alecs");
-    //cout << "Conteudo da trie " << endl;
-    //display(root, str, level);
-
-    //cout << "Imprimindo os prefixos" << endl;
-    //printAutoSuggestions(root, "neuro");
-
     int opcao;
-    struct TrieNode *root = getNode();
+    struct noTrie *raiz = novoNo();
 
     while (2 == 2)
     {
@@ -311,6 +344,7 @@ int main()
         printf("2. Consultar palavra\n");
         printf("3. Imprimir todas as palavras\n");
         printf("4. Remover livro(stopwords)\n");
+        printf("5. Bonus\n");
         printf("0. Sair\n");
         printf("----------------------------------------------\n");
 
@@ -320,17 +354,17 @@ int main()
         {
 
         case 1:
-            ClearTerminal();
-            inserir_via_arquivo_txt(root);
+            limparTerminal();
+            inserir_via_arquivo_txt(raiz);
             printf("Opcao 1\n");
             break;
         case 2:
-            ClearTerminal();
+            limparTerminal();
             printf("Opcao 2\n");
             char palavra[256];
             cin >> palavra;
             int opc;
-            opc = printAutoSuggestions(root, palavra);
+            opc = consultar_palavra(raiz, palavra);
             if (opc == 0)
             {
                 cout << "Nao foi encontrado" << endl;
@@ -338,16 +372,22 @@ int main()
             }
             break;
         case 3:
-            ClearTerminal();
+            limparTerminal();
             printf("Opcao 3\n");
-            display(root, str, level);
+            imprimir(raiz, str, level);
             break;
         case 4:
-            ClearTerminal();
-            //  exclui_livro();
+            limparTerminal();
+            stopwords(raiz);
+            break;
+        case 5:
+            limparTerminal();
+            //void imprimirSimilar(  int level,int distancia, char palavra[]){
+ 
+            imprimirSimilar(raiz, "", 1,2,"escreve");
             break;
         case 0:
-            ClearTerminal();
+            limparTerminal();
             printf("\nPrograma finalizado\n");
             exit(0);
             break;
